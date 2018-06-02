9a56c163ac4e0e3467265befda327792403e6a37
Avery Regier <avery.regier@gmail.com>
2018-06-02 16:50:20 -0500

    version 3
    The readme for v3 refers to v2.1. ¯\_(ツ)_/¯

--- src/WONDER.C
+++ src/WONDER.C
@@ -5,7 +5,6 @@
 #include <time.h>
 #include <dos.h>
 
-
 main(void)
 {
 	int driver, mode;
@@ -13,59 +12,77 @@ main(void)
 	int repeat, now;
 	int addsx, addsy, addex, addey;
 	int sign, num;
-	int colors, hcolr;
 	int ch;
 	unsigned int msec;
 	int bk;
 	int stover, rstover;
 	int col;
-
+	int max,addx,addy;
+     int space;
+	int errorcode;
 	int intro(void);
+
      intro();
 
 	msec=0;
 	bk=1;
 	stover=0;
-
-	#define MAXX getmaxy()
-	#define MAXY getmaxy()
+	space=3;
 
 	driver = DETECT;
 	mode = 0;
 	initgraph(&driver, &mode, "");
+	errorcode=graphresult();
+	if( errorcode != grOk ){
+		printf(" Graphics System Error: %s\n", grapherrormsg( errorcode ) );
+		exit(1);
+	}
 
-     #define MAXCOLOR getmaxcolor()+1
+	if(getmaxx()>getmaxy()) {
+		max=getmaxy();
+		addx=(getmaxx()-getmaxy())/2;
+		addy=0;
+	}
+	else {
+		max=getmaxx();
+		addx=0;
+          	addy=(getmaxy()-getmaxx())/2;
+	}
+
+	#define MAX max
+	#define MAXCOLOR getmaxcolor()+1
+	#define ADDX addx
+     	#define ADDY addy
+
 	randomize();
+     col=random(MAXCOLOR);
 	for(;;) {
-		if(ch=='b' || ch=='c' || ch=='B' || ch=='C') {
+		if(ch=='b' || ch=='c' || ch=='B' || ch=='C' || ch=='I' || ch=='i') {
 			setbkcolor(random(bk));
 		}
 		loop:
-		rstover=random(20);
-		moveto(random(MAXX), random(MAXY));
+		rstover=random(20+space);
+		moveto(random(MAX), random(MAX));
 		startx=getx();
 		starty=gety();
-		endx=startx+random(5);
-		endy=starty+random(5);
-          col=random(MAXCOLOR);
+		endx=startx;
+		endy=starty;
 		do {
 			for(num=0; num<5; num++) {
 				sign=random(2);
-				if(sign==0) {
-                    	sign=-1;
-				}
+				if(sign==0) sign=-1;
 				switch(num) {
 					case 0:
-						addsx=random(3)*sign;
+						addsx=random(space)*sign;
 						break;
 					case 1:
-						addsy=random(3)*sign;
+						addsy=random(space)*sign;
 						break;
 					case 2:
-						addex=random(3)*sign;
+						addex=random(space)*sign;
 						break;
 					case 3:
-						addey=random(3)*sign;
+						addey=random(space)*sign;
 						break;
 					case 4:
 						setcolor(col+sign*random(3));
@@ -74,23 +91,24 @@ main(void)
 			}
 			repeat=random(20);
 			for(now=1; now<repeat; now++) {
-				if(startx>=MAXX || endx>=MAXX || starty>=MAXY || endy>=MAXY || startx<=0 || starty <=0 || endx <=0 || endy<=0) {
+				if(startx>=MAX || endx>=MAX || starty>=MAX || endy>=MAX || startx<=0 || starty <=0 || endx <=0 || endy<=0) {
 					stover++;
 					if(stover>=rstover) {
 						cleardevice();
 						stover=0;
 						setbkcolor(random(bk));
+                         	col=random(MAXCOLOR);
                          }
 					goto loop;
 				}
-				line(startx, starty, endx, endy);
-		 		line(MAXX-startx, MAXY-starty, MAXX-endx, MAXY -endy);
-				line(starty, startx, endy, endx);
-				line(MAXX-starty, MAXY-startx, MAXX-endy, MAXY-endx);
-				line(startx, MAXY-starty, endx, MAXY-endy);
-				line(MAXX-startx, starty, MAXX-endx, endy);
-				line(starty, MAXY-startx, endy, MAXY-endx);
-				line(MAXX-starty, startx, MAXX-endy, endx);
+				line(startx+ADDX, starty+ADDY, endx+ADDX, endy+ADDY);
+				line(MAX-startx+ADDX, MAX-starty+ADDY, MAX-endx+ADDX, MAX-endy+ADDY);
+				line(starty+ADDX, startx+ADDY, endy+ADDX, endx+ADDY);
+				line(MAX-starty+ADDX, MAX-startx+ADDY, MAX-endy+ADDX, MAX-endx+ADDY);
+				line(startx+ADDX, MAX-starty+ADDY, endx+ADDX, MAX-endy+ADDY);
+				line(MAX-startx+ADDX, starty+ADDY, MAX-endx+ADDX, endy+ADDY);
+				line(starty+ADDX, MAX-startx+ADDY, endy+ADDX, MAX-endx+ADDY);
+				line(MAX-starty+ADDX, startx+ADDY, MAX-endy+ADDX, endx+ADDY);
 				delay(msec);
 				startx+=addsx;
 				starty+=addsy;
@@ -99,64 +117,78 @@ main(void)
 			}
 		} while(!kbhit());
 		ch=getch();
-		tolower(ch);
 		switch(ch) {
-			case '0':
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9':
-				msec=(ch-48)*5;
-                    break;
-			case 72:
-				if(msec>0){
-					msec-=5;
-				} else{
-					sound(440);
-                         delay(100);
-					nosound();
-                    }
-				break;
-			case 80:
-				if(msec<=50){
-					msec+=5;
+          		case 75:
+				if(space>2){
+					space-=1;
 				} else{
 					sound(440);
 					delay(100);
 					nosound();
 				}
 				break;
-               case 'C':
-			case 'c':
-				cleardevice();
-				stover=0;
+			case 77:
+				if(space<=10){
+					space+=1;
+				} else{
+					sound(440);
+					delay(100);
+					nosound();
+				}
 				break;
-               case 'B':
-			case 'b':
-				if(bk==1) {
-					bk=16;
-				} else bk=1;
+			default:
+				ch=tolower(ch);
+				switch(ch) {
+					case '0':
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+					case '9':
+						msec=(ch-48)*5;
+						break;
+					case 'c':
+						cleardevice();
+						stover=0;
+						break;
+					case 'b':
+						if(bk==1) bk=MAXCOLOR;
+						else bk=1;
+						break;
+					case 'p':
+						getch();
+						break;
+					case 'h':
+						restorecrtmode();
+						intro();
+						setgraphmode(getgraphmode());
+						break;
+					case 'd':
+						bk=1;
+						space=3;
+						msec=0;
+						setbkcolor(0);
+						break;
+					case 0x1b:
+						restorecrtmode();
+						return 0;
+				}
 				break;
-			case 'p':
-				getch();
-				break;
-			case 0x1b:
-				restorecrtmode();
-				return 0;
 		}
 	}
 }
 
 int intro(void)
 {
+	char ch;
+
 	clrscr();
 	printf("                                WONDER\n\r");
-	printf("                              Version 1.0\n\r");
+	printf("                              Version 3.0\n\r");
 	printf("                            Copyright 1993\n\r");
 	printf("                             Avery Regier\n\r");
 	printf("                           cAVEman Software\n\r\n\n");
@@ -164,13 +196,18 @@ int intro(void)
 	printf("               Send your name, address, and comments to:\n\r");
 	printf("                           3311 6th Avenue\n\r");
 	printf("                        Sioux City, IA  51106\n\r");
-	printf("                       Compuserve [71214,1301]\n\r\n\n\n");
-	printf("		     'B'         toggles background\n\r");
-	printf("		     'C'         clears the screen and starts over\n\r");
-	printf("		     'P'         pauses the screen\n\r");
-	printf("		     '0'-'9'     sets the speed\n\r             (0 is full speed, 9 is the slowest speed)\n\r");
-	printf("                     ESC key     quit\n\r");
+	printf("                       Compuserve [71214,1301]\n\r\n");
+	printf("               'B'         toggles background\n\r");
+	printf("               'C'         clears the screen and starts over\n\r");
+	printf("               'D'         set options to default\n\r");
+	printf("               'H'         this screen\n\r");
+	printf("               'P'         pauses the screen\n\r");
+	printf("               '0'-'9'     sets the speed (0 is maximum speed)\n\r");
+	printf("               right & left\n\r");
+	printf("               arrow keys  set the space between lines\n\r");
+	printf("               ESC key     quit\n\r");
 	printf("\n\r                       Press any key to start.\n\r");
-	getch();
+	ch=getch();
+	if(ch==0x1b) exit(0);
 	return 0;
 }
